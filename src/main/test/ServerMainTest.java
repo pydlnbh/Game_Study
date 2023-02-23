b6614c420e26bcf471cab6ce55fe4f2c45578b63
peiyiding <616256541@qq.com>
2023-02-24 00:46:19 +0800

    modify code

---
+++ src/main/test/ServerMainTest.java
@@ -0,0 +1,54 @@
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+
+public class ServerMainTest {
+    public static void main(String[] args) {
+        // 实例化bossGroup(拉客的美女)
+        NioEventLoopGroup boosGroup = new NioEventLoopGroup();
+        // 实例化workGroup(服务员小哥)
+        NioEventLoopGroup workGroup = new NioEventLoopGroup();
+
+        // 实例化启动器
+        ServerBootstrap bootstrap = new ServerBootstrap();
+
+        // 把boosGroup和workGroup通过group方法放入启动器中
+        bootstrap.group(boosGroup, workGroup);
+        // 通过channel方法给启动器设置NioServerSocketChannel.class
+        bootstrap.channel(NioServerSocketChannel.class);
+        // 通过childHandler初始化channel
+        bootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            protected void initChannel(SocketChannel socketChannel) throws Exception {
+                // 通过pipeline的addLast方法加入一些初始化参数
+                socketChannel.pipeline().addLast(
+                        // Http服务器编解码
+                        new HttpServerCodec(),
+                        // 内容长度限制, 两个字节
+                        new HttpObjectAggregator(65535),
+                        // websocket协议
+                        new WebSocketServerProtocolHandler("/websocket")
+                );
+            }
+        });
+
+        try {
+            // 使用启动器绑定端口, 一定使用sync()方法, 负责启动不成功
+            ChannelFuture channelFuture = bootstrap.bind(1234).sync();
+
+            if (channelFuture.isSuccess()) {
+                System.out.println("服务器启动成功");
+            }
+
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
